29324c1f75d579b0904dfde5f048271719f8a336
nandanimaddheshiya278-ux <nandanimaddheshiya278@gmail.com>
2026-08-13 00:18:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,10 @@
-from flask import Flask, render_template, request
-import joblib
-import pandas as pd
-import os
-
+from flask import Flask
 app = Flask(__name__)
-
-# Model ka correct absolute path
-MODEL_PATH = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)),
-    "customer_churn_model.pkl"
-)
-
-
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "Flask app is working!"
 
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    model=joblib.load(MODEL_PATH)
 
-    tenure = float(request.form["tenure"])
-    monthly = float(request.form["MonthlyCharges"])
-    total = float(request.form["TotalCharges"])
-
-    contract = request.form["Contract"]
-    payment = request.form["PaymentMethod"]
-    internet = request.form["InternetService"]
-    security = request.form["OnlineSecurity"]
-    support = request.form["TechSupport"]
-
-    input_data = pd.DataFrame({
-        "tenure": [tenure],
-        "MonthlyCharges": [monthly],
-        "TotalCharges": [total],
-        "Contract": [contract],
-        "PaymentMethod": [payment],
-        "InternetService": [internet],
-        "OnlineSecurity": [security],
-        "TechSupport": [support]
-    })
-
-    prediction = model.predict(input_data)[0]
-
-    if prediction == 1:
-        result = "⚠️ Customer is likely to Churn"
-    else:
-        result = "✅ Customer is likely to Stay"
-
-    return render_template(
-        "index.html",
-        prediction_text=result
-    )
-
-
-if __name__ == "__main__":
-    app.run()
+    
+        
