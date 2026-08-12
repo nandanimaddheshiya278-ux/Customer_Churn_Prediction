497317e0bfa68c858d01cc7dbb0add10a6472aab
nandanimaddheshiya278-ux <nandanimaddheshiya278@gmail.com>
2026-08-13 00:23:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Flask app is working!"
+    return
+render_template("index.html")    
 
 
 
