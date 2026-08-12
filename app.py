3bdc378ffe4a44ac572c31244d96893aedc6effa
nandanimaddheshiya278-ux <nandanimaddheshiya278@gmail.com>
2026-08-13 00:36:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
 from flask import Flask, render_template
-import joblib
-import os
 
 app = Flask(__name__)
-
-MODEL_PATH = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)),
-    "customer_churn_model.pkl"
-)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
-@app.route("/test-model")
-def test_model():
-    try:
-        model = joblib.load(MODEL_PATH)
-        return "MODEL LOADED SUCCESSFULLY ✅"
-    except Exception as e:
-        return f"MODEL ERROR ❌: {str(e)}"
+@app.route("/test")
+def test():
+    return "Vercel Flask is working ✅"
